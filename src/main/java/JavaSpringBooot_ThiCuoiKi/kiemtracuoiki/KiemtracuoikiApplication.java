cd3c911926907e359cf1ca46f241d1e927304921
Phan Anh Minh <phananhminh24@gmail.com>
2024-11-28 21:31:30 +0700

    Source init

---
+++ src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/KiemtracuoikiApplication.java
@@ -0,0 +1,13 @@
+package JavaSpringBooot_ThiCuoiKi.kiemtracuoiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiemtracuoikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiemtracuoikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/KiemtracuoikiApplicationTests.java
@@ -0,0 +1,13 @@
+package JavaSpringBooot_ThiCuoiKi.kiemtracuoiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiemtracuoikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
