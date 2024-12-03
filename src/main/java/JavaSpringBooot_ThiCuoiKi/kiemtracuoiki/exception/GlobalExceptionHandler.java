cecef44cd47bceffde24fb8272546645cb8fcf66
Nguyen160900 <nguyen4224hn@gmail.com>
2024-12-03 22:42:12 +0700

    Update Source

--- src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/exception/GlobalExceptionHandler.java
+++ src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package JavaSpringBooot_ThiCuoiKi.kiemtracuoiki.exception;
 
+import JavaSpringBooot_ThiCuoiKi.kiemtracuoiki.pojo.ApiResult;
+import lombok.extern.log4j.Log4j2;
 import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,17 +9,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Log4j2
 public class GlobalExceptionHandler {
     //Bắt lỗi 500
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleUnwantedException(Exception e) {
+    public ResponseEntity<ApiResult<ErrorCode>> handleUnwantedException(Exception e) {
         e.printStackTrace();
-        return ResponseEntity.status(500).body("Unknow error");
+        return ResponseEntity.status(500).body(ApiResult.error(ErrorCode.SYSTEM_ERROR));
     }
 
     // Bắt lỗi 400
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> handleBadRequestException(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    public ResponseEntity<ApiResult<ErrorCode>> handleBadRequestException(Exception e, ErrorCode errorCode) {
+        log.info("Bad Request: {}", e.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ApiResult.error(errorCode));
     }
 }
--- src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/pojo/ApiResult.java
+++ src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/pojo/ApiResult.java
@@ -15,11 +15,19 @@ public class ApiResult<T> {
     String message;
     HttpStatus httpStatus;
 
+    public ApiResult(T results) {
+        this.results = results;
+    }
+
     public static <T> ApiResult<T> success(T data) {
         return new ApiResult<>(data, CommonConstants.SUCCESS, HttpStatus.OK);
     }
 
     public static <T> ApiResult<T> success(T data, String message) {
         return new ApiResult<>(data, message, HttpStatus.OK);
+    }
+
+    public static <T> ApiResult<T> error(T data) {
+        return new ApiResult<>(data);
     }
 }
