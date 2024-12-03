2911f46340558dbf194f4b73d1cd329e53d9129f
Nguyen160900 <nguyen4224hn@gmail.com>
2024-12-03 21:55:01 +0700

    Update Source

--- src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/pojo/request/SignupRequest.java
+++ src/main/java/JavaSpringBooot_ThiCuoiKi/kiemtracuoiki/pojo/request/SignupRequest.java
@@ -1,23 +1,20 @@
 package JavaSpringBooot_ThiCuoiKi.kiemtracuoiki.pojo.request;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class SignupRequest {
-     String username;
-     String password;
-     String email;
-     String fullName;
-     String phoneNumber;
-     String address;
-     String profileImage;
-     String roleId;
-
+    String username;
+    String password;
+    String email;
+    String fullName;
+    String phoneNumber;
+    String address;
+    String profileImage;
+    String roleId;
 }
